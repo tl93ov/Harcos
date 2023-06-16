2932a3f7ab3b62936eaa33d1c129e41801cd29e6
TL93OV <jixin1207@gmail.com>
2023-06-16 19:20:09 +0200

    - Két objektum létrehozása: Harcos és Varazslo - Egy uj szál létrehozása: harcThread, a szál működése egy while ciklusban található. - Amíg mind2 karakter életben van a harcos támadja a varázslót, majd a varázsló a harcost - A harc szála elindul: harc.Tread.start() meghívással - try-catch blokk megvárja amig a harc szál befejeződik harcThread.join() al vagy kivétel esetén a catch blokkba lépés lenne... - a győztes karakter kiírása if-else használatával - A Harcos és a Varaázsló osztályok valósítják meg a karaktereket: név, életerő, iklletve hogy életben van-e, amit a isEltbenVan() metódus csekkol - mind2 osztályban van 'tamad' metódus ami  egy tamadast vegez a masik karakter ellen - A megSerul() metodus hivasa hajtja vegre a karakterek sebzeset ami kiirodik a konzol ablakban - A Harcos és Varazslo osztalyon belül a megSerul() vegrehajtja a karakter sebzeset: levonja az egeszsegbol, ha negativ nullara allitja - a synchronized kulcsszó biztosítja, hogy a megSerul() metódust csak egy szál tudja egyszerre végrehajtani
    Megjegyzés: Azt hogy pályán mozognak, ott azt a feltételezést veszem alapul, hogy egy pálya, az egy szint, amin a karakterek harcolnak és egyikük veszít, míg a másik nyer.
    megpróbáltam X Y koordinátákkal implementálni, külön fájlokba rendezve a harcost, varászlót, stb, de totál szétesett a projekt időnyomás alatt nem tudom megcsinálni :)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,94 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Harcos harcos = new Harcos("Harcos", 100);
+        Varazslo varazslo = new Varazslo("Varázsló", 80);
+
+        /* a harcot elindul külön szálon*/
+        Thread harcThread = new Thread(() -> {
+            while (harcos.isEletbenVan() && varazslo.isEletbenVan()) {
+                harcos.tamad(varazslo);
+                varazslo.tamad(harcos);
+            }
+        });
+        harcThread.start();
+
+        /* a harc befejeződését várjuk*/
+        try {
+            harcThread.join();
+        } catch (InterruptedException e) {
+        }
+
+        /* a győztes kiírása*/
+        if (harcos.isEletbenVan()) {
+            System.out.println(harcos.getNev() + " győzött!");
+        } else {
+            System.out.println(varazslo.getNev() + " győzött!");
+        }
+    }
+}
+
+class Harcos {
+    private final String nev;
+    private int egeszseg;
+
+    public Harcos(String nev, int egeszseg) {
+        this.nev = nev;
+        this.egeszseg = egeszseg;
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public boolean isEletbenVan() {
+        return egeszseg > 0;
+    }
+
+    public void tamad(Varazslo varazslo) {
+        /* egy támadás*/
+        varazslo.megSerul(20);
+        System.out.println(nev + " megtámadta " + varazslo.getNev() + "t.");
+    }
+
+    public synchronized void megSerul(int serules) {
+        /* egy sebzés*/
+        egeszseg -= serules;
+        if (egeszseg < 0) {
+            egeszseg = 0;
+        }
+        System.out.println(nev + " kapott " + serules + " sebzést.");
+    }
+}
+
+class Varazslo {
+    private final String nev;
+    private int egeszseg;
+
+    public Varazslo(String nev, int egeszseg) {
+        this.nev = nev;
+        this.egeszseg = egeszseg;
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public boolean isEletbenVan() {
+        return egeszseg > 0;
+    }
+
+    public void tamad(Harcos harcos) {
+        /*egy támadás megvalosul*/
+        harcos.megSerul(15);
+        System.out.println(nev + " megtámadta " + harcos.getNev() + "t.");
+    }
+
+    public synchronized void megSerul(int serules) {
+        /*egy  sebzés megvalosul*/
+        egeszseg -= serules;
+        if (egeszseg < 0) {
+            egeszseg = 0;
+        }
+        System.out.println(nev + " kapott " + serules + " sebzést.");
     }
 }
